054adda1e3cf60e617a8d0529532bb0c0d4dc38c
Marian Serna <marianhalliday@gmail.com>
2017-06-02 08:29:16 -0400

    Created public folder + Starter Sass ITCSS - BEM structure

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ if (module.hot) {
 }
 
 // Bring in the sass file
-import './style/style.scss';
+import './scss/main.scss';
+
 // Get the URL to an image
 import codeURL from './images/code.png'
 
@@ -12,10 +13,10 @@ import codeURL from './images/code.png'
 const root = document.querySelector('#root');
 root.innerHTML = `<p>Hello webpack.</p>`;
 
-// Create an image using the codeURL image from above
-const img = document.createElement('img');
-img.src = codeURL;
-img.style.backgroundColor = "#2B3A42";
-img.style.padding = "20px";
-img.width = 32;
-document.body.appendChild(img);
+// Eg: Create an image using the codeURL image from above
+// const img = document.createElement('img');
+// img.src = codeURL;
+// img.style.backgroundColor = "#2B3A42";
+// img.style.padding = "20px";
+// img.width = 32;
+// document.body.appendChild(img);
